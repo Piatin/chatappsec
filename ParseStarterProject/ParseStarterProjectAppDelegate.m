6723abe620f3dbdee640a65a14e6bb76d2e776e1
Kako <Kako@Toyoshima-no-MacBook-Air.local>
2015-03-04 16:54:19 +0900

    add nurup

--- ParseStarterProject/ParseStarterProjectAppDelegate.m
+++ ParseStarterProject/ParseStarterProjectAppDelegate.m
@@ -34,6 +34,7 @@
     //
     // Uncomment and fill in with your Parse credentials:
      [Parse setApplicationId:@"your_application_id" clientKey:@"your_client_key"];
+    //ぬるp
     //
     // If you are using Facebook, uncomment and add your FacebookAppID to your bundle's plist as
     // described here: https://developers.facebook.com/docs/getting-started/facebook-sdk-for-ios/
